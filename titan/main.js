a7e20283ec3ce15b1d70ef9740df924310ee60e7
kazifarhanuddin <kazifarhan199@gmail.com>
2019-10-28 13:48:29 +0530

    Added better product page

---
+++ titan/main.js
@@ -0,0 +1,40 @@
+// Main
+
+    // For nav bar  
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
+
+    // For Popup    
+    $(document).ready(function () {
+        $('.modal').modal();
+    });
+
+    // For Parallax
+    $(document).ready(function () { 
+        $('.parallax').parallax();
+    });
+
+// Product-detail
+    let img = document.getElementById('main-img');
+    var imgs = document.querySelectorAll('.images')
+    let no_of = document.getElementById('no_of')
+    function changeImg(src) {
+    img.src=src
+    }
+    for(i=0; i<imgs.length; i++) {
+        let v = imgs[i].src;
+        imgs[i].addEventListener("click", function(){changeImg(v)}, false);
+    }
+    function decrease_product() {
+    if (Number(no_of.value) > 1){
+        no_of.value = Number(no_of.value)-1
+    }
+    }
+    function increase_product() {
+    no_of.value = Number(no_of.value) + 1        
+    }
+    // For tabs
+    $(document).ready(function(){
+    $('.tabs').tabs();
+    });
